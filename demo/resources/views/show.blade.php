cc3ada5bf5bebed0080b94ee9e61575d8286459a
tomoya <tomocha.marika@gmail.com>
2019-06-08 19:27:25 +0900

    詳細ページのデザインを変更

--- demo/resources/views/show.blade.php
+++ demo/resources/views/show.blade.php
@@ -2,42 +2,28 @@
 @section('content')
 <body class="bg-white show">
   <h2 class="p-2">ROOM</h1>
-    <div class="container">
-      <div class="row justify-content-between text-center mb-5">
-        <!-- <div class="card col-5">
-        <div class="card-title">上村莉奈</div>
-        <ul>
-        <li>aaaasageha2454(コントラクトのアドレス)</li>
-      </ul>
-      <img src="img/test.jpg" class="w-100">
-
-    </div> -->
-    <!-- <div class="col-5 heart">
-    <p>出会って50日</p>
-    <i class="fas fa-heart text-center"></i>
-  </div> -->
-  <div class="card col-6">
-    <img src="img/test.jpg" class="w-100">
-    <div class="card-title">上村莉奈</div>
-    <ul>
-      <li>aaaasageha2454(コントラクトのアドレス)</li>
-    </ul>
-    <i class="fas fa-heart text-center"></i>
-  </div>
-  <!-- <div class="col-5 heart">
-    <p>出会って50日</p>
-    <i class="fas fa-heart text-center"></i>
-  </div> -->
-  <div class="card col-6">
-    <img src="img/test.jpg" class="w-100">
-    <div class="card-title">上村莉奈</div>
-    <ul>
-      <li>aaaasageha2454(コントラクトのアドレス)</li>
-    </ul>
-  </div>
-</div>
-<img src="img/qr.jpg" class="w-100">
-</div>
-</body>
-</html>
-@endsection
+    <div class="container text-center">
+      <h3 class="mb-5">つきあって30日だよ</h3>
+      <div class="row justify-content-between mb-5">
+        <div class="card col-6">
+          <img src="img/test.jpg" class="w-100">
+          <div class="card-title">上村莉奈</div>
+          <i class="fas fa-heart text-center"></i>
+        </div>
+        <div class="card col-6">
+          <img src="img/test.jpg" class="w-100">
+          <div class="card-title">上村莉奈</div>
+        </div>
+      </div>
+      <div class="w-100 mt-5">
+          <h3 class="mb-5">二人が初めてつきあった日は<br>3月12日</h3>
+      </div>
+      <div class="qr">
+      <p>みんなにシェア！</p>
+      <i class="fas fa-arrow-down"></i>
+    </div>
+      <img src="img/qr.jpg" class="w-100">
+    </div>
+  </body>
+  </html>
+  @endsection

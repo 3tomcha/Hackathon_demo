2b2fe5144d47e83fcea8ac22c930f7a442379387
tomoya <tomocha.marika@gmail.com>
2019-06-08 18:52:45 +0900

    詳細ページのデザインを変更

--- demo/resources/views/show.blade.php
+++ demo/resources/views/show.blade.php
@@ -1,20 +1,43 @@
 @extends('layouts/app')
 @section('content')
-<body>
-  <div class="container">
-    <div class="row justify-content-between">
-      <div class="card col-md-5">
+<body class="bg-white show">
+  <h2 class="p-2">ROOM</h1>
+    <div class="container">
+      <div class="row justify-content-between text-center mb-5">
+        <!-- <div class="card col-5">
         <div class="card-title">上村莉奈</div>
-        <img src="img/test.jpg" class="w-100">
-        aaaasageha2454
-      </div>
-      <div class="card col-md-5">
-        <div class="card-title">上村莉奈</div>
-        <img src="img/test.jpg" class="w-100">
-        aaaasageha2454
-      </div>
-    </div>
+        <ul>
+        <li>aaaasageha2454(コントラクトのアドレス)</li>
+      </ul>
+      <img src="img/test.jpg" class="w-100">
+
+    </div> -->
+    <!-- <div class="col-5 heart">
+    <p>出会って50日</p>
+    <i class="fas fa-heart text-center"></i>
+  </div> -->
+  <div class="card col-6">
+    <img src="img/test.jpg" class="w-100">
+    <div class="card-title">上村莉奈</div>
+    <ul>
+      <li>aaaasageha2454(コントラクトのアドレス)</li>
+    </ul>
+    <i class="fas fa-heart text-center"></i>
   </div>
+  <!-- <div class="col-5 heart">
+    <p>出会って50日</p>
+    <i class="fas fa-heart text-center"></i>
+  </div> -->
+  <div class="card col-6">
+    <img src="img/test.jpg" class="w-100">
+    <div class="card-title">上村莉奈</div>
+    <ul>
+      <li>aaaasageha2454(コントラクトのアドレス)</li>
+    </ul>
+  </div>
+</div>
+<img src="img/qr.jpg" class="w-100">
+</div>
 </body>
 </html>
 @endsection
